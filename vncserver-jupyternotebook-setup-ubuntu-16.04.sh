b394286d08816611f82ad17d46c98cbe4a3f8366
mandar <mandar.d@quantinsti.com>
2018-08-13 15:42:59 +0530

    added script to setup vncserver and jupyter notebook

---
+++ vncserver-jupyternotebook-setup-ubuntu-16.04.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+set -e
+
+#installing packages
+
+SCRIPT_USER=$USER
+SCRIPT_HOME=$HOME
+
+printf "\nInstalling required packages\n" 
+
+sudo apt update && sudo apt -y install xfce4 xfce4-goodies xfonts-base xfonts-75dpi xfonts-100dpi tightvncserver python2.7 python-pip
+
+sudo python2.7 -m pip install jupyter 
+
+
+#killing services / processes
+
+if systemctl is-active vncserver@1.service; then \
+   sudo systemctl stop vncserver@1.service && printf "\vncserver@1.service stopped\n"; \
+elif pgrep vnc; then \
+	sudo pkill vnc;
+fi
+
+if systemctl is-active jupyter-notebook.service; then \
+   sudo systemctl stop jupyter-notebook.service && printf "\jupyter-notebook.service stopped\n"; \
+elif pgrep jupyter; then \
+	sudo pkill jupyter;
+fi
+
+#setting up vnc server
+
+touch $SCRIPT_HOME/.Xresources 
+
+if [ ! -f $SCRIPT_HOME/.vnc/passwd ]; then 
+	printf "\nEnter Password for VNC Dekstop\n" 
+fi
+
+
+
+vncserver :1 && vncserver -kill :1
+
+if [ ! -f $SCRIPT_HOME/.vnc/xstartup.bk ]; then
+	mv  $SCRIPT_HOME/.vnc/xstartup $SCRIPT_HOME/.vnc/xstartup.bk
+fi
+
+cat > $SCRIPT_HOME/.vnc/xstartup << EOL
+#!/bin/sh
+
+xrdb $HOME/.Xresources
+xsetroot -solid grey
+/etc/X11/Xsession
+export XKL_XMODMAP_DISABLE=1
+
+EOL
+
+chmod a+x $SCRIPT_HOME/.vnc/xstartup
+
+
+
+
+
+
+#setting up jupyter notebook
+if [ ! -f $SCRIPT_HOME/.jupyter/jupyter_notebook_config.py ]; then
+	jupyter notebook --generate-config
+fi
+
+if [ ! -f $SCRIPT_HOME/.jupyter/jupyter_notebook_config.json ]; then
+	printf "\nEnter Password for Jupyter notebook\n" && jupyter notebook password
+fi
+
+printf "\nAdding services for VNC server and Jupyter Notebook\n"
+
+sudo bash -c "cat >  /etc/systemd/system/vncserver@.service << EOL
+[Unit]
+Description=Start TightVNC server at startup
+After=syslog.target network.target
+
+[Service]
+Type=forking
+User=${SCRIPT_USER}
+PAMName=login
+PIDFile=${SCRIPT_HOME}/.vnc/%H:%i.pid
+ExecStartPre=-/usr/bin/vncserver -kill :%i > /dev/null 2>&1
+ExecStart=/usr/bin/vncserver  :%i
+ExecStop=/usr/bin/vncserver -kill :%i
+Restart=always
+RestartSec=10
+
+[Install]
+WantedBy=multi-user.target
+
+EOL"
+
+
+sudo bash -c "cat >  /etc/systemd/system/jupyter-notebook.service << EOL
+[Unit]
+Description=Jupyter Notebook
+After=syslog.target network.target
+
+[Service]
+Type=simple
+User=${SCRIPT_USER}
+PIDFile=${SCRIPT_HOME}/.jupyter/jupyter.pid
+ExecStart=/usr/local/bin/jupyter notebook --config=${SCRIPT_HOME}/.jupyter/jupyter_notebook_config.py  --no-browser --ip=0.0.0.0 --port=8888
+WorkingDirectory=${SCRIPT_HOME}
+Restart=always
+RestartSec=10
+
+
+[Install]
+WantedBy=multi-user.target
+
+EOL"
+
+sudo systemctl daemon-reload \
+&& sudo systemctl enable vncserver@1.service && sudo systemctl enable jupyter-notebook.service \
+&& sudo systemctl start vncserver@1  && sudo systemctl start jupyter-notebook.service
